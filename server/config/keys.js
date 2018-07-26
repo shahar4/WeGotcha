0bc937daac00110cc089e5b0adf8946a01e4271b
Shahar Kaminsky <skaminsky@sailthru.com>
2018-07-26 12:56:34 -0400

    Authentication fails.

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
     googleClientID: '317236553141-2kff6bi7ieaot4po4ospkp49p6i5ujfl.apps.googleusercontent.com',
     googleClientSecret: 'OToMyZ2Iiuk6vlS3EEUxrWws',
+    mongoURI: 'mongodb://ShaharKaminsky:mlabsk4120@ds255451.mlab.com:55451/wegotcha-db',
+    cookieKey: 'hellothererandomstring',
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -9,8 +22,20 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback',
         },
-        (accessToken) => {
-            console.log(accessToken);
+        (accessToken, refreshToken, profile, done) => {
+            console.log("profile:/n ", profile);
+            new User({ googleId: profile.id })
+                .save()
+                .then(user => done(null, user));
+            // User.findOne({ googleId: profile.id }).then((existingUser) => {
+            //     if (existingUser) {
+            //         done(null, existingUser);
+            //     } else {
+            //         new User({ googleId: profile.id })
+            //             .save()
+            //             .then(user => done(null, user));
+            //     }
+            // });
         }
     )
 );
